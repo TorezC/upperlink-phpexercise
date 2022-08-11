c918eee747e38f01a6da426e506fee4184be5f31
TorezC <victorytorez@gmail.com>
2022-08-11 15:13:34 +0100

    completed assignment

--- app/App.php
+++ app/App.php
@@ -3,3 +3,56 @@
 declare(strict_types = 1);
 
 // Your Code
+
+function getFile() {
+    $openFile = fopen("../transaction_files/sample_1.csv", "r");
+
+    while (($transact = fgetcsv($openFile, 200, ","))) {
+        $array[] = $transact;
+    };
+
+    fclose($openFile);
+
+    return $array;
+}
+
+function getTotals() {
+    $transaction = getFile();
+    $transactionLen = count($transaction);
+    $sumOfIncome = 0;
+    $sumOfExpense = 0;
+    
+    for ($i=1; $i<$transactionLen; $i++) {
+        $lastInd = count($transaction[$i]) - 1;
+        $arrValue = $transaction[$i][$lastInd];
+        if (!str_starts_with($arrValue, "-")) {       // check if positive value
+            $incomeValue = trim_transaction($arrValue);
+            $sumOfIncome += $incomeValue;
+        } else {
+            $expenseValue = trim_transaction($arrValue);
+            $sumOfExpense += $expenseValue;
+        }
+    }
+    $netTotal = $sumOfIncome+ $sumOfExpense;
+    
+    $sumOfIncome = number_format($sumOfIncome, 2);      // round up to 2 d.p
+
+    $sumOfExpense = number_format($sumOfExpense, 2);
+    $sumOfExpense = str_replace('-', '', $sumOfExpense);        // remove the '-' from expenses
+    
+    $netTotal = number_format($netTotal, 2);
+
+    $totalsArr = array($sumOfIncome, $sumOfExpense, $netTotal);
+
+    return $totalsArr;
+}
+
+function trim_transaction($transaction) {
+    $transaction = str_replace('$', '', $transaction);  
+    $transaction = str_replace(',', '', $transaction);  
+    $transaction = (float)$transaction;       
+
+    return $transaction;
+};
+
+?>
--- views/transactions.php
+++ views/transactions.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 <html>
     <head>
@@ -21,6 +22,12 @@
             tfoot tr th {
                 text-align: right;
             }
+            .expense {
+                color: red;
+            }
+            .income {
+                color: green;
+            }
         </style>
     </head>
     <body>
@@ -35,19 +42,51 @@
             </thead>
             <tbody>
                 <!-- YOUR CODE -->
+             <?php
+                  include '../app/App.php';
+                    $transactions = getFile();
+                    $transactionsLen = count($transactions);
+                    $totalIncome = getTotals()[0];
+                    $totalExpense = getTotals()[1];
+                    $netTotal = getTotals()[2];
+
+                    for($i = 1; $i<$transactionsLen; $i++) {
+                        $itemLen = count($transactions[$i]);
+                        $row = "";
+                        $lastIndex = $itemLen - 1;
+                        for ($j = 0; $j<$itemLen; $j++) {
+                            if ($j === 0) {                 // check if 1st column and change date format if true
+                                $arrDate = $transactions[$i][$j];
+                                $arrDate = explode('/', $arrDate);
+                                $setDate =  date("M d, Y", mktime(0,0,0,$arrDate[0],$arrDate[1],$arrDate[2]));
+                                $row .= "<td>". $setDate . "</td>";
+                            }
+                            elseif ($j !== $lastIndex) {        // check if it's not the last column
+                                $row .= "<td>". $transactions[$i][$j]."</td>";
+                            } else {
+                                if (str_starts_with($transactions[$i][$j], '-')) {      // check if it's an expense and add red color if true
+                                    $row .= "<td class='expense'>". $transactions[$i][$j]."</td>";
+                                } else {                                        // check if it's an income and add green color if true
+                                    $row .= "<td class='income'>". $transactions[$i][$j]."</td>";
+                                }
+                            }
+                        }
+                        echo "<tr>". $row . "</tr>";
+                    }
+                ?>
             </tbody>
             <tfoot>
                 <tr>
                     <th colspan="3">Total Income:</th>
-                    <td><!-- YOUR CODE --></td>
+                    <?php echo "<td>$". $totalIncome . "</td>"; ?>
                 </tr>
                 <tr>
                     <th colspan="3">Total Expense:</th>
-                    <td><!-- YOUR CODE --></td>
+                    <?php echo "<td>-$". $totalExpense . "</td>"; ?>
                 </tr>
                 <tr>
                     <th colspan="3">Net Total:</th>
-                    <td><!-- YOUR CODE --></td>
+                    <?php echo "<td>$" . $netTotal . "</td>" ?>
                 </tr>
             </tfoot>
         </table>
